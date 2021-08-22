50db694d181a84445ebed390e273fa54433019f9
BEN SALAH Abderrahman <40049722+xDEVELYNNx@users.noreply.github.com>
2021-08-22 07:37:05 +0100

    Initial commit

---
+++ movie-catalog-service/src/test/java/com/movieNote/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieNote.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
