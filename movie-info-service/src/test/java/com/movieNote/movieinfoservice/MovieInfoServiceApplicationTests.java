6237be105d1cbf92cb3c78fa4c89a15c9dab8583
BEN SALAH Abderrahman <40049722+xDEVELYNNx@users.noreply.github.com>
2021-08-22 07:41:54 +0100

    Initial commit

---
+++ movie-info-service/src/test/java/com/movieNote/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieNote.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
